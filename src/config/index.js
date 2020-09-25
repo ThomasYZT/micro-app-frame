45dea3f6611ad4303e2fc79fa0df9c77d3298886
thomasYZT <624524240@qq.com>
2020-09-25 11:45:33 +0800

    feat: 登录跳转域名修改

--- src/config/index.js
+++ src/config/index.js
@@ -8,11 +8,11 @@ const config = {
 switch (HTTP_ENV) {
   case 'test':
     config.APPID = 'wx853a8d12eea0e682';
-    config.AUTHURL = 'https://piaoquan.yishihui.com/';
+    config.AUTHURL = 'https://piaoquantv.yishihui.com/';
     break;
   case 'pre':
     config.APPID = 'wx853a8d12eea0e682';
-    config.AUTHURL = 'https://piaoquan.yishihui.com/';
+    config.AUTHURL = 'https://pre.piaoquantv.com/';
     break;
   case 'prod':
     config.APPID = 'wx4ad82552268afc8d';
