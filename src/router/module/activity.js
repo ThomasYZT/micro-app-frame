73ed88cd128f213b9cb9db71624a10d569e50a0f
thomasYZT <624524240@qq.com>
2020-12-01 21:06:42 +0800

    路由修改

--- src/router/module/activity.js
+++ src/router/module/activity.js
@@ -7,11 +7,11 @@ export default [
   {
     path: '/ac201124',
     name: 'ac201124',
-    component: () => import(/* webpackChunkName: "home" */ '../../pages/activity/ac201124.vue')
+    component: () => import(/* webpackChunkName: "ac201124" */ '../../pages/activity/ac201124.vue')
   },
   {
     path: '/ac201201',
     name: 'ac201201',
-    component: () => import(/* webpackChunkName: "home" */ '../../pages/activity/ac201201.vue')
+    component: () => import(/* webpackChunkName: "ac201201" */ '../../pages/activity/ac201201.vue')
   }
 ];
